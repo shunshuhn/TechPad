c2dd6a5d27d121ac1ba5b75ef6850b6b93d5ea87
HARUKA ito <haruka_ito@keio.jp>
2020-09-14 23:52:24 +0900

    TechPad

--- TechPad/AppDelegate.swift
+++ TechPad/AppDelegate.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //playbackground
+        
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playback, mode: .default)
+        } catch{
+            //エラー処理
+            fatalError("カテゴリ設定失敗")
+        }
+        //sessionのアクティブ化
+        do {
+            try session.setActive(true)
+        }catch {
+            //audio sessionの有効化失敗の処理
+            //ここではエラーとして停止している
+            fatalError("sesion有効化失敗")
+        }
+        
         return true
     }
 
--- TechPad/ViewController.swift
+++ TechPad/ViewController.swift
@@ -7,13 +7,83 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    //StoryBoardで扱うTableViewを宣言
+    @IBOutlet var table: UITableView!
+    
+    //曲名を入れるための配列
+    var songNameArray = [String]()
+    
+    //曲のファイル名を入れるための配列
+    var fileNameArray = [String]()
+    
+    //音楽家の画像を入れるための配列
+    var imageNameArray = [String]()
+    
+    //音楽を再生するための変数
+    var audioPlayer : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //テーブルビューのデータソースメソッドはViewControllerクラスに書くよ、という設定
+        table.dataSource = self
+        
+        //テーブルビューのデリゲートメソッドはViewControllerメソッドに書くよ、という設定
+        //UITableViewが持っているDalegateメソッドの処理の委託先をself(ViewCountroller.swift)に設定
+        table.delegate = self
+        
+        //songNameArrayに曲名を入れていく
+        songNameArray = ["カノン", "エリーゼのために", "G戦場のアリア"]
+        
+        //fileNameArrayにファイル名を入れていく
+        fileNameArray = ["cannon", "elise", "aria"]
+        
+        //imageNameArrayに画像を入れていく
+        imageNameArray = ["Beethoven.jpg", "Pachelbel.jpg", "Bach.jpg",]
     }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //セルの数をsongNameArrayの数にする
+        return songNameArray.count
+    }
+    
+    //ID付きのセルを取得して、セル付属のtextlabelに「テスト」と表示させてみる
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        //セルにsongNameArrayの曲名を表示する
+        cell?.textLabel?.text = songNameArray[indexPath.row]
+        
+        //セルに音楽家の画像を表示する
+        cell?.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
+        return cell!
+    
+    }
+    
+    //セルが押された時に呼ばれるdelegateメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(songNameArray[indexPath.row])が選ばれました")
+        
+        
+        //音楽ファイルの設定
+        let audioPath = URL(fileURLWithPath: Bundle.main.path(forResource:fileNameArray[indexPath.row], ofType: "mp3")!)
+        
+        //再生の準備
+        audioPlayer = try? AVAudioPlayer(contentsOf: audioPath)
+        
+     //音楽を再生
+        audioPlayer.play()
+        
+    }
+    
 
 
 }
